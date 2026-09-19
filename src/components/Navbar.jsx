e6b60422233b10d301a0eee4dde3e5ac4f916094
Emy-gold <imanelgarate@gmail.com>
2026-09-19 19:45:00 +0100

    start doing the nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,46 @@
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
+
 export default function Navbar() {
 
-    return <div></div>;
+    const [mobileMenuIsOpen, setMobileMenuIsOpen] = useState(false);
+
+    return <nav className="fixed top-0 w-full z-50 transition-all duration-300 bg-slate-950/20 backdrop-blur-sm">
+        <div className="max-w-7xl mx-auto px-4 lg:px-8 sm-px-6">
+            <div className="flex justify-between items-center h-14 sm:h-16 md:h-20">
+                <div className="flex items-center space-x-1 group cursor-pointer">
+                    <div>
+                        <img src="/logo.png" className="w-6 h-6 sm:w-8 sm:h-8" alt="MyLogo" />
+                    </div>
+                    <span className="text-lg sm:text-xl md:text-2xl font-medium">
+                        <span className="text-white">Code</span>
+                        <span className="text-blue-400">Flow</span>
+                    </span>
+                </div>
+                {/* Nav Links*/}
+                <div className="md:flex hidden items-center space-x-6 lg:space-x-8 ">
+                    <a
+                        href="#features"
+                        className="text-gray-300 hover:text-white text-small lg:text-base">
+                        Features
+                    </a>
+                    <a
+                        href="#pricing"
+                        className="text-gray-300 hover:text-white text-small lg:text-base">
+                        Pricing
+                    </a>
+                    <a
+                        href="#testimonials"
+                        className="text-gray-300 hover:text-white text-small lg:text-base">
+                        Testimonials
+                    </a>
+                </div>
+                <button className="md:hidden p-2 text-gray-300 hover:text-white"
+                    onClick={() => setMobileMenuIsOpen((prev) => !prev)}
+                >
+                    {mobileMenuIsOpen ? (<X className="w-5 h-5 sm:h-6 sm:w-6" />) : (<Menu className="w-5 h-5 sm:h-6 sm:w-6" />)}
+                </button>
+            </div>
+        </div>
+    </nav>;
 }
